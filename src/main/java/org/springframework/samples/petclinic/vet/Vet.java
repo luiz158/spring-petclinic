cba3e693970d68387b875f92e6fe3a4ed49adfc6
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-06-03 11:40:13 +0300

    Fix performance issues reported by Hypersistence Optimizer

--- src/main/java/org/springframework/samples/petclinic/vet/Vet.java
+++ src/main/java/org/springframework/samples/petclinic/vet/Vet.java
@@ -45,7 +45,7 @@ import org.springframework.samples.petclinic.model.Person;
 @Table(name = "vets")
 public class Vet extends Person {
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties;
 
